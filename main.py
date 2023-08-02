d17d4cefeafb6f25a5a391f4806222def065be10
jjcet <jjcetnarski@gmail.com>
2023-08-02 14:16:23 +0200

    Data Ingestion Stage added

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from TextSummary.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummary.logging import logger
 
-logger.info("Starting TextSummary")
 
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(STAGE_NAME + " started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(STAGE_NAME + " completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/TextSummary/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,18 @@
+from TextSummary.config.configuration import ConfigurationManager
+from TextSummary.components.data_ingestion import DataIngestion
+from TextSummary.logging import logger
+
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+
+        config =  ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
