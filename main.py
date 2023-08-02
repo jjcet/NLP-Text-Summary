05865c495d258508be40c4f0ab31334c1992078c
jjcet <jjcetnarski@gmail.com>
2023-08-02 16:16:49 +0200

    Data Transformation Added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from TextSummary.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummary.pipeline.stage_o2_data_validation import DataValidationTrainingPipeline
+from TextSummary.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 from TextSummary.logging import logger
 
 
@@ -21,6 +23,19 @@ try:
     logger.info(STAGE_NAME + " started")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(STAGE_NAME + " completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(STAGE_NAME + " started")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(STAGE_NAME + " completed")
 except Exception as e:
     logger.exception(e)
---
+++ src/TextSummary/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,16 @@
+
+from TextSummary.config.configuration import ConfigurationManager
+from TextSummary.components.data_transformation import DataTransformation
+
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        config =  ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
