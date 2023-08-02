10574ef6520e8ba29cf3da2d0a02a60d4588446d
jjcet <jjcetnarski@gmail.com>
2023-08-02 15:22:05 +0200

    Data Validation components

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from TextSummary.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummary.pipeline.stage_o2_data_validation import DataValidationTrainingPipeline
 from TextSummary.logging import logger
 
 
@@ -9,6 +10,17 @@ try:
     logger.info(STAGE_NAME + " started")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(STAGE_NAME + " completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(STAGE_NAME + " started")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(STAGE_NAME + " completed")
 except Exception as e:
     logger.exception(e)
